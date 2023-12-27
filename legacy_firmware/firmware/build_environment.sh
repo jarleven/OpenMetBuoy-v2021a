64c3be03f430057be21aa1c2cb23d3ca0d8eee23
jarleven <jarleven@gmail.com>
2023-12-27 16:52:52 +0100

    Update build_environment.sh
    Should work for anyone now

--- legacy_firmware/firmware/build_environment.sh
+++ legacy_firmware/firmware/build_environment.sh
@@ -13,6 +13,11 @@
 # Log serialport output :   screen /dev/ttyUSB0 1000000
 # Exit screen with :        ctrl-A k y
 #
+# Arduino CLI 
+#    arduino-cli version
+#    arduino-cli board list
+#
+#
 #  TODO: Fix for all users Install arduino-cli
 #  TODO: Fix serialport 
 
@@ -25,40 +30,37 @@ sudo usermod -a -G dialout $USER
 
 sudo apt-get install -y curl git build-essential ffmpeg arduino-mk && sudo apt-get clean
 
-### Install not done """ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/usr/local/bin sh
-### TODO : I tried different stuff and I'm uncertain what actually worked for me....
+# From Arduino-CLI, slightly different than in the Dockerfile
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 
-/home/jarleven/bin/arduino-cli config init
+arduino-cli config init
 
+arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/sparkfun/Arduino_Apollo3/main/package_sparkfun_apollo3_index.json
 
- /home/jarleven/bin/arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/sparkfun/Arduino_Apollo3/main/package_sparkfun_apollo3_index.json
+arduino-cli core update-index
+arduino-cli core install "Sparkfun:apollo3@1.2.1"
 
-
-
-/home/jarleven/bin/arduino-cli core update-index
-/home/jarleven/bin/arduino-cli core install "Sparkfun:apollo3@1.2.1"
-
-/home/jarleven/bin/arduino-cli lib update-index
+arduino-cli lib update-index
 
 
 # TODO arduino-cli folder
 # TODO arduino library folder
 #  $HOME/ should work !
 
-/home/jarleven/bin/arduino-cli lib install "Embedded Template Library ETL"
-touch /home/jarleven/Arduino/libraries/Embedded_Template_Library_ETL/src/etl.h
-/home/jarleven/bin/arduino-cli lib install Time
-/home/jarleven/bin/arduino-cli lib install "SparkFun Qwiic Power Switch Arduino Library"
-/home/jarleven/bin/arduino-cli lib install "SparkFun u-blox GNSS Arduino Library@2.0.2"
-/home/jarleven/bin/arduino-cli lib install IridiumSBDi2c
-/home/jarleven/bin/arduino-cli lib install "Adafruit LSM6DS@4.3.1"
-/home/jarleven/bin/arduino-cli lib install "Adafruit BusIO@1.7.2"
-/home/jarleven/bin/arduino-cli lib install "Adafruit LIS3MDL"
-/home/jarleven/bin/arduino-cli lib install "Adafruit AHRS@2.2.4"
-/home/jarleven/bin/arduino-cli lib install "OneWire@2.3.6"
+arduino-cli lib install "Embedded Template Library ETL"
+touch $HOME/Arduino/libraries/Embedded_Template_Library_ETL/src/etl.h
+arduino-cli lib install Time
+arduino-cli lib install "SparkFun Qwiic Power Switch Arduino Library"
+arduino-cli lib install "SparkFun u-blox GNSS Arduino Library@2.0.2"
+arduino-cli lib install IridiumSBDi2c
+arduino-cli lib install "Adafruit LSM6DS@4.3.1"
+arduino-cli lib install "Adafruit BusIO@1.7.2"
+arduino-cli lib install "Adafruit LIS3MDL"
+arduino-cli lib install "Adafruit AHRS@2.2.4"
+arduino-cli lib install "OneWire@2.3.6"
 
 
-cd /home/jarleven/Arduino/libraries
+cd $HOME/Arduino/libraries
 rm -rf Adafruit_BusIO
 git clone -b "fix/SPI_with_Artemis" --depth=1 https://github.com/jerabaul29/Adafruit_BusIO.git
 
@@ -68,31 +70,29 @@ git clone -b "feat/propagate_bool_flags" --depth=1 https://github.com/jerabaul29
 rm -rf OneWire
 git clone -b "feat/Artemis_compatible" --depth=1 https://github.com/jerabaul29/OneWire.git
 
-cd ~
+cd $HOME
 
 git clone --depth=1 https://github.com/jerabaul29/OpenMetBuoy-v2021a.git
-
 
 cd OpenMetBuoy-v2021a/legacy_firmware/firmware/functionality_test_mode/
 mv tracker.ino functionality_test_mode.ino
 
-/home/jarleven/bin/arduino-cli compile -v -b SparkFun:apollo3:artemis .
+arduino-cli compile -v -b SparkFun:apollo3:artemis .
 
-
-
-root@fb25dd462854:/work/OpenMetBuoy-v2021a/legacy_firmware/firmware/scanner# arduino-cli compile -v -b SparkFun:apollo3:amap3redboard .
-
-
-sudo docker run -it --device=/dev/ttyUSB0 omb_compiler:latest /bin/bash
-
-	/home/jarleven/bin/arduino-cli compile -v -b SparkFun:apollo3:amap3redboard .
-/home/jarleven/bin/arduino-cli upload --port /dev/ttyUSB0 -v -b SparkFun:apollo3:amap3redboard .
-
-screen /dev/ttyUSB0 1000000
-ctrl-A k y
-
-
-sudo apt remove brltty
-
-root@8249e876ee23:/work/OpenMetBuoy-v2021a/legacy_firmware/firmware/scanner# arduino-cli version
-arduino-cli  Version: 0.34.2 Commit: 963c1a76 Date: 2023-09-11T10:05:42Z
+#
+# root@fb25dd462854:/work/OpenMetBuoy-v2021a/legacy_firmware/firmware/scanner# arduino-cli compile -v -b SparkFun:apollo3:amap3redboard .
+#
+#
+# sudo docker run -it --device=/dev/ttyUSB0 omb_compiler:latest /bin/bash
+#
+# arduino-cli compile -v -b SparkFun:apollo3:amap3redboard .
+# arduino-cli upload --port /dev/ttyUSB0 -v -b SparkFun:apollo3:amap3redboard .
+#
+# screen /dev/ttyUSB0 1000000
+# ctrl-A k y
+#
+#
+#
+# root@8249e876ee23:/work/OpenMetBuoy-v2021a/legacy_firmware/firmware/scanner# arduino-cli version
+# arduino-cli  Version: 0.34.2 Commit: 963c1a76 Date: 2023-09-11T10:05:42Z
+#
