6bc83803706e5fb076c61fceac97518a3f4732c7
jarleven <jarleven@gmail.com>
2023-12-28 13:25:25 +0100

    Create alt_build_environment.sh

---
+++ legacy_firmware/firmware/alt_build_environment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Libraries from the Artemis OpenLog project
+
+arduino-cli lib install "SdFat@2.2.0"
+arduino-cli lib install "SparkFun 9DoF IMU Breakout - ICM 20948 - Arduino Library"
