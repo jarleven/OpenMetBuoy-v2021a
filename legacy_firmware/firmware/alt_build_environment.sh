116c5c1e2800fe0022b172b45dc6135d225617fb
jarleven <jarleven@gmail.com>
2024-01-02 15:12:13 +0100

    Update alt_build_environment.sh

--- legacy_firmware/firmware/alt_build_environment.sh
+++ legacy_firmware/firmware/alt_build_environment.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+
+sudo apt-get update
+sudo apt-get upgrade -y 
+sudo apt remove -y brltty
+# Rebooting works best after changing user rights on the serialport
+sudo usermod -a -G tty $USER
+sudo usermod -a -G dialout $USER
+
+sudo apt-get install -y curl git build-essential ffmpeg arduino-mk && sudo apt-get clean
+
 sudo apt-get install python3-pip
 python3 -m pip install icecream numpy matplotlib scipy   
+
+# From Arduino-CLI, slightly different than in the Dockerfile
+cd $HOME
+rm install.sh
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+
+arduino-cli config init
+
+arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/sparkfun/Arduino_Apollo3/main/package_sparkfun_apollo3_index.json
+
+# TODO: Comments are from functionality test mode legacy build (Original build)
+arduino-cli core update-index
+arduino-cli core install "Sparkfun:apollo3@1.2.1"
+# Using board 'artemis' from platform in folder: /home/jrmet/.arduino15/packages/SparkFun/hardware/apollo3/1.2.1
+# Using core 'arduino' from platform in folder: /home/jrmet/.arduino15/packages/SparkFun/hardware/apollo3/1.2.1
+
+arduino-cli lib update-index
+
+
 
 #TODO:
 #  Using later versions of Arduino 
